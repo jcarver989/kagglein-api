b66ff1333211ba77a50e90496dfca779eaefec64
jcarver <jcarver@linkedin.com>
2016-05-20 15:27:42 -0700

    fix typo

--- messages.rb
+++ messages.rb
@@ -1,5 +1,5 @@
 module Messages
-  def self.wrong_number_of_guesses(answer_size, guesses_size)
+  def self.wrong_number_of_guesses(answers_size, guesses_size)
     "The test data set has #{answers_size} rows, but you submitted guesses for #{guesses_size} rows. Since you made a boobo, this wont count against your API limit"
   end
 
